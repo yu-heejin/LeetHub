3504b3ab89bf7449ac4e45559153108eaae4ab45
Yu Hee Jin <96467030+yu-heejin@users.noreply.github.com>
2022-07-05 18:58:00 +0900

    [Bronze V] Title: A+B - 3, Time: 232 ms, Memory: 17988 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int x = sc.nextInt();   //case는 예약어이기 때문에 변수명으로 사용하면 안됨
-        for(int i=0; i<x; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            
-            System.out.println(a + b);
+        int x = sc.nextInt();
+        int[] num = new int[x * 2];
+        
+        for(int i=0; i<x * 2; i++) {
+            num[i] = sc.nextInt();
+        }
+        
+        for(int i=0; i<x*2; i+=2) {
+            System.out.println(num[i] + num[i+1]);
         }
     }
 }
