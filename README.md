35bd64df73a61ede46734dd4c80acc4b696c3831
yu-heejin <jkgt0521@naver.com>
2023-11-02 23:49:27 +0900

    폴더 정리

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# boj
-This is a auto push repository for Baekjoon Online Judge created with [BaekjoonHub](https://github.com/BaekjoonHub/BaekjoonHub).
+LeetHub
--- 백준/Bronze/2557. Hello World/Hello World.java
+++
@@ -1,2 +0,0 @@
-import java.io.*;
-public class Main{public static void main(String[] args) throws IOException{BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));bw.write("Hello World!");bw.flush();bw.close();}}
