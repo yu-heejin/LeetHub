f9db24ee6bee9d3f8dec6121ee44515e2aea017e
yu-heejin <96467030+yu-heejin@users.noreply.github.com>
2023-12-15 08:44:55 +0900

    Time: 671 ms (49.81%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,9 @@
+SELECT CASE
+        WHEN id = (SELECT MAX(id) FROM seat) AND MOD(id, 2) = 1 THEN id
+        WHEN MOD(id, 2) = 1 THEN id + 1
+        ELSE id - 1
+    END AS id
+    , student
+FROM seat
+ORDER BY id
+;
