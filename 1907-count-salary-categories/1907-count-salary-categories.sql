73d228a1591baf72f700fa7fa54a37dd0c0f1290
hoody <yuheejin0914@gmail.com>
2024-10-28 23:29:04 +0900

    Time: 1671 ms (61.76%), Space: 0B (100.00%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,23 +1,26 @@
-# 각 월급 별 카테고리
-WITH AccountsCategory AS (
-    SELECT CASE WHEN income < 20000 THEN 'Low Salary'
-        WHEN income >= 20000 AND income <= 50000 THEN 'Average Salary'
+WITH accounts_per_category AS (
+    SELECT *
+        , CASE
         WHEN income > 50000 THEN 'High Salary'
-        ELSE null
+        WHEN income <= 50000 AND income >= 20000 THEN 'Average Salary'
+        WHEN income < 20000 THEN 'Low Salary'
         END AS category
     FROM Accounts
 ),
-Category AS (
-    SELECT 'Low Salary' AS category
-    UNION
-    SELECT 'Average Salary' AS category
-    UNION
+category_const AS (
     SELECT 'High Salary' AS category
+    UNION ALL
+    SELECT 'Average Salary' AS category
+    UNION ALL
+    SELECT 'Low Salary' AS category
 )
 
 SELECT C.category
-    , COUNT(AC.category) AS accounts_count
-FROM AccountsCategory AC
-    RIGHT JOIN Category C ON AC.category = C.category
-GROUP BY C.category
-;
+    , IFNULL(accounts_count, 0) AS accounts_count
+FROM (
+    SELECT category
+        , COUNT(*) AS accounts_count
+    FROM accounts_per_category
+    GROUP BY category
+) A
+RIGHT JOIN category_const C ON A.category = C.category;
