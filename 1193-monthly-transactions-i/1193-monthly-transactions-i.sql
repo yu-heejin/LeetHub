24b11d95996c6f220c66aedc311df3d18816713d
hoody <yuheejin0914@gmail.com>
2024-11-22 00:56:12 +0900

    Time: 819 ms (18.94%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,10 +1,19 @@
-SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month
+# 월별 거래 분류 테이블
+WITH TransactionByMonth AS (
+    SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month
+        , country
+        , state
+        , amount
+    FROM Transactions
+)
+
+SELECT month
     , country
     , COUNT(*) AS trans_count
-    , COUNT(IF(state = 'approved', 1, NULL)) AS approved_count
+    , COUNT(IF(state = 'approved', state, null)) AS approved_count
     , SUM(amount) AS trans_total_amount
     , SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
-FROM Transactions
-GROUP BY DATE_FORMAT(trans_date, '%Y-%m')
-    , country
+FROM TransactionByMonth
+GROUP BY country, month
 ;
+    
