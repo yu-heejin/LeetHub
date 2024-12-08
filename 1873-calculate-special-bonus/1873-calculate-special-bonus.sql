af8ba0f352291b2bc50a737e65ed148aa084f97e
hoody <yuheejin0914@gmail.com>
2024-12-08 14:57:37 +0900

    Time: 500 ms (99.59%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,5 +1,5 @@
 SELECT employee_id
-    , IF(employee_id % 2 != 0 AND name NOT LIKE 'M%', salary, 0) AS bonus
+    , IF(LEFT(name, 1) != 'M'
+            AND employee_id % 2 != 0, salary, 0) AS bonus
 FROM Employees
-ORDER BY employee_id
-;
+ORDER BY employee_id;
