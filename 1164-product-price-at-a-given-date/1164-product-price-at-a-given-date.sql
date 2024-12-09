0f72851dda6052ad67572091bbdf9228d1a8aefa
hoody <yuheejin0914@gmail.com>
2024-12-09 22:53:59 +0900

    Time: 519 ms (60.47%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,19 +1,20 @@
-WITH product_id_table AS (
+# 2019-08-16 까지의 가격을 찾기
+WITH OriginalPrice AS (
+    SELECT product_id
+        , new_price
+        , change_date
+        , RANK() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS date_rank
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+),
+ProductIds AS (
     SELECT DISTINCT product_id
     FROM Products
-),
-change_date_table AS (
-    SELECT PI.product_id
-        , MAX(change_date) AS change_date
-    FROM Products P
-        RIGHT JOIN product_id_table PI ON P.product_id = PI.product_id
-        AND P.change_date <= '2019-08-16'
-    GROUP BY product_id
 )
 
-SELECT CT.product_id
-    , IF(P.new_price IS NULL, 10, P.new_price) AS price
-FROM change_date_table CT
-    LEFT JOIN Products P ON CT.product_id = P.product_id
-    AND CT.change_date = P.change_date
+SELECT P1.product_id
+    , IFNULL(P2.new_price, 10) AS price
+FROM ProductIds P1
+    LEFT JOIN OriginalPrice P2 ON P1.product_id = P2.product_id
+    AND P2.date_rank = 1
 ;
