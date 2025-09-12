39e24957c10340fa486a471f437f56689a99e2d9
Ellie <yuheejin0914@gmail.com>
2025-09-12 18:40:44 +0900

    Time: 473 ms (56.83%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,9 +1,14 @@
+# MAX는 집계 결과가 없으면 항상 null을 반환한다.
+-- SELECT MAX(customer_number) AS customer_number
+-- FROM Orders;
+
+# 가장 많은 주문을 한 고객넘버
 SELECT customer_number
 FROM (
-    SELECT COUNT(order_number) AS order_count
+    SELECT COUNT(*) AS count
         , customer_number
     FROM Orders
     GROUP BY customer_number
-) R
-ORDER BY order_count DESC
+) T
+ORDER BY count DESC
 LIMIT 1;
